4348ceeb05c55b66cde8eba2f214aa80c156d765
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-19 05:11:28 +0000

    version 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/edd-helpscout/tags/1.0.3@1093988 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ edd-helpscout.php
@@ -0,0 +1,140 @@
+<?php
+/*
+Plugin Name: Easy Digital Downloads integration for HelpScout
+Plugin URI: https://dannyvankooten.com/helpscout-edd
+Description: Easy Digital Downloads integration for HelpScout
+Version: 1.0.3
+Author: Danny van Kooten
+Author URI: https://dannyvankooten.com
+Text Domain: edd-helpscout
+Domain Path: /languages
+License: GPL v3
+
+Easy Digital Downloads integration for HelpScout
+Copyright (C) 2012-2013, Danny van Kooten, hi@dannyvankooten.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Prevent direct file access
+if ( ! defined( 'ABSPATH' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit;
+}
+
+class EDD_HS {
+
+	/**
+	 * @const VERSION
+	 */
+	const VERSION = "1.0.3";
+
+	/**
+	 * @const FILE
+	 */
+	const FILE = __FILE__;
+
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+
+		// do nothing if EDD is not activated
+		if( ! class_exists( 'Easy_Digital_Downloads', false ) ) {
+			return;
+		}
+
+		// register autoloader
+		spl_autoload_register( array( $this, 'autoload' ) );
+
+		// if this is a HelpScout Request, load the Endpoint class
+		if ( $this->is_helpscout_request() && ! is_admin() ) {
+			new EDD_HS_Endpoint();
+		}
+
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+			new EDD_HS_Ajax();
+		} elseif( is_admin() ) {
+			new EDD_HS_Admin();
+		}
+	}
+
+	/**
+	 * Is this a request we should respond to?
+	 *
+	 * @return bool
+	 */
+	private function is_helpscout_request() {
+
+		$trigger = stristr( $_SERVER['REQUEST_URI'], '/edd-hs-api/customer-data.json' ) !== false;
+
+		if( ! $trigger && isset( $_SERVER['HTTP_X_HELPSCOUT_SIGNATURE'] ) ) {
+
+			$greedy = get_option( 'edd_hs_greedy_listening', 1 );
+
+			if( $greedy ) {
+				$trigger = true;
+			}
+		}
+
+		return (bool) apply_filters( 'edd_hs/is_helpscout_request', $trigger );
+	}
+
+	/**
+	 * @param string $class
+	 *
+	 * @return bool
+	 */
+	public function autoload( $class ) {
+
+		// only act on classnames with this prefix
+		if( strpos( $class, 'EDD_HS_' ) !== 0 ) {
+			return false;
+		}
+
+		$filename = dirname( __FILE__ ) . '/includes/class-' . strtolower( substr( $class, 7 ) ) . '.php';
+
+		if( file_exists( $filename ) ) {
+			require_once( $filename );
+			return true;
+		}
+
+		return false;
+	}
+
+}
+
+/**
+ * Initiate the EDD_HS class
+ */
+function __load_edd_helpscout() {
+	new EDD_HS;
+}
+
+/**
+ * Disable greedy listening for new plugin users
+ */
+function edd_hs_disable_greedy_listening() {
+	update_option( 'edd_hs_greedy_listening', 0 );
+}
+
+
+// Instantiate the plugin on a later hook
+add_action( 'plugins_loaded', '__load_edd_helpscout', 90 );
+
+// Register activation hook
+register_activation_hook( __FILE__, 'edd_hs_disable_greedy_listening' );
+
---
+++ includes/class-ajax.php
@@ -0,0 +1,90 @@
+<?php
+
+if( ! defined("EDD_HS::VERSION") ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit;
+}
+
+/**
+ * This class takes care of AJAX requests from HelpScout
+ */
+class EDD_HS_Ajax {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		// Double add_action because we want it to work when you're logged in too
+		add_action( 'wp_ajax_nopriv_hs_action', array( $this, 'ajax_action' ) );
+		add_action( 'wp_ajax_hs_action', array( $this, 'ajax_action' ) );
+	}
+
+
+	/**
+	 * Handle AJAX actions
+	 */
+	public function ajax_action() {
+
+		// Verify request
+		if( isset( $_GET['s'] ) ) {
+			$given_signature = isset( $_GET['s'] ) ? $_GET['s'] : '';
+			unset( $_GET['s'] );
+		} else {
+			$given_signature = '';
+		}
+
+		$request = new EDD_HS_Request( $_GET );
+
+		// verify signature and referrer
+		if( ! $request->signature_equals( $given_signature )  || ! $request->referred_from_helpscout() ) {
+			die( '-1' );
+		}
+
+		switch ( $_REQUEST['hs_action'] ) {
+			case 'deactivate':
+				$this->handle_deactivation_request();
+				break;
+			case 'purchase-receipt':
+				$this->handle_purchase_receipt_resend();
+				break;
+			default:
+				break;
+		}
+
+		die('<script>window.close();</script>');
+	}
+
+	/**
+	 * Deactivates a site
+	 */
+	private function handle_deactivation_request() {
+		$license_id = sanitize_text_field( $_REQUEST['license_id'] );
+		$site_url = sanitize_text_field( $_REQUEST['site_url'] );
+
+		edd_software_licensing()->delete_site( $license_id, $site_url );
+	}
+
+	/**
+	 * Handle resending the purchase email.
+	 */
+	private function handle_purchase_receipt_resend() {
+		$purchase_id = absint( $_REQUEST['order'] );
+
+		edd_email_purchase_receipt( $purchase_id, false );
+
+		// Grab all downloads of the purchase and update their file download limits, if needed
+		// This allows admins to resend purchase receipts to grant additional file downloads
+		$downloads = edd_get_payment_meta_downloads( $purchase_id );
+
+		if ( is_array( $downloads ) ) {
+			foreach ( $downloads as $download ) {
+				$limit = edd_get_file_download_limit( $download['id'] );
+				if ( ! empty( $limit ) ) {
+					edd_set_file_download_limit_override( $download['id'], $purchase_id );
+				}
+			}
+		}
+
+	}
+}
